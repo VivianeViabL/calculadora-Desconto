f7ebdf6331522095b64f70d48dad322df4e0f83c
VivianeViabL <viviane.viaboni.l@gmail.com>
2026-02-16 00:48:58 -0300

    Commit CSS 01

---
+++ exercicio3.js
@@ -0,0 +1,15 @@
+
+function calculadoraDesconto(){
+    
+    //Entrada
+    let valorOriginal = document.getElementById("valor").value; // "let" é variável. No JavaScript, é mais utilizado o "let" do que o "var"
+    let desconto = document.getElementById("desconto").value;
+    //Processamento
+    let valorDesconto = (valorOriginal * desconto) / 100;
+    let valorFinal = valorOriginal - valorDesconto;
+
+    document.getElementById("resultado").textContent = "Resultado: " + valorFinal; // o "textContent" nesse caso vai substituir o id "resultado" pela variável "valorFinal"
+    //Saída
+}
+
+
